4cf8d606906a5eff431ed4d07d023247bd84da32
pattylo <19451945as@gmail.com>
2025-07-07 20:22:58 +0800

    now about to do tcp udp sending

--- fo1/fo1.ino
+++ fo1/fo1.ino
@@ -3,24 +3,37 @@
 const int joint1_servo_pin = 9;
 const int joint1_feedback_pin = A0;
 float theta1 = 0;
+int joint1_pulse = 0;
+Servo servo1;
 
 const int joint2_servo_pin = 10;
 const int joint2_feedback_pin = A1;
 float theta2 = 0;
+int joint2_pulse = 0;
+Servo servo2;
 
 const int joint3_servo_pin = 11;
 const int joint3_feedback_pin = A2;
 float theta3 = 0;
+int joint3_pulse = 0;
+Servo servo3;
 
 float system_lastrequest = 0;
 bool system_wake = false;
+
+float ctrl_lastrequest = 0;
 
 String input_from_serial_link;
 String output_to_serial_link;
 
 void setup() {
     system_lastrequest = millis();
+    ctrl_lastrequest = millis();
     Serial.begin(9600);
+
+    servo1.attach(joint1_servo_pin);
+    servo2.attach(joint2_servo_pin);
+    servo3.attach(joint3_servo_pin);
 }
 
 void loop() {
@@ -33,13 +46,35 @@ void loop() {
     else if (input_from_serial_link.toInt() == 8888)
     {
         system_wake = false;
-    }
+    }    
     else
     {
-        // do control shit here
-    }                                        
+        // do control shit here -> decode shit
+        input_from_serial_link;
+        int firstDash = input_from_serial_link.indexOf('-');
+        int secondDash = input_from_serial_link.indexOf('-', firstDash + 1);
 
-    if ( (millis() - system_lastrequest) > 10)
+            if (firstDash != -1 && secondDash != -1) 
+            {
+                joint1_pulse = input_from_serial_link.substring(0, firstDash).toInt();
+                joint2_pulse = input_from_serial_link.substring(firstDash + 1, secondDash).toInt();
+                joint3_pulse = input_from_serial_link.substring(secondDash + 1).toInt();
+            }
+    }
+
+    if(
+        (millis() - ctrl_lastrequest) > 20
+    )
+    {
+        servo1.writeMicroseconds(joint1_pulse);
+        servo2.writeMicroseconds(joint2_pulse);
+        servo3.writeMicroseconds(joint3_pulse);        
+    }
+
+    // this is for sending to upper floor
+    if ( 
+        (millis() - system_lastrequest) > 10
+    )
     {
         output_to_serial_link = "";
 
@@ -49,7 +84,16 @@ void loop() {
             theta2 = analogRead(joint1_feedback_pin);
             theta3 = analogRead(joint1_feedback_pin);
 
-            output_to_serial_link = String(millis()) + "-" + theta1 + "-" + theta2 + "-" + theta3 + "\n";
+            output_to_serial_link = 
+                String(millis()) + "-" + 
+                1 + "-" + 
+                theta1 + "-" + 
+                theta2 + "-" + 
+                theta3 + "-" + 
+                joint1_pulse + "-" +
+                joint2_pulse + "-" +
+                joint3_pulse + "-" +
+                "\n";
         }            
         else
             output_to_serial_link = String(millis()) + "-0-0-0-0\n";
--- serial_link/src/serial_link.cpp
+++ serial_link/src/serial_link.cpp
@@ -8,16 +8,25 @@
 #include <thread> 
 
 #include <csignal>
+#include <vector>
+#include <sstream>
+#include <algorithm>
 
 volatile std::sig_atomic_t stop_flag;
 
 void handle_sigint(int signum);
-void joint_callback();
 void joint_spinner();
-void process_pot(const char buf[]);
+void joints_callback(const char buf[]);
 void process_pwm();
+double chrono_to_double(const std::chrono::nanoseconds time_chrono);
+bool is_digits(const std::string& s);
 
-static bool system_start = false;
+static std::vector<double> message_from_down;
+static bool system_on = false;
+static double dt_ms;
+static int t_ard_ms;
+static int fsm;
+static int pots_raw[3];
 
 int main() {
 
@@ -61,14 +70,17 @@ int main() {
 
     std::signal(SIGINT, handle_sigint);
 
+    fsm = 0;
+
     while (!stop_flag) 
     {
+        // 1. first process buffer from serial
         int n = read(fd, buf, sizeof(buf) - 1);
 
         if (n > 0) 
         {
             buf[n] = 0;
-            process_pot(buf);
+            joints_callback(buf);
         } 
         else if (n == 0 || (errno == EAGAIN || errno == EWOULDBLOCK))         
             continue;
@@ -78,22 +90,80 @@ int main() {
             break;
         }
         
-        auto dt_raw = std::chrono::high_resolution_clock::now() - ctrl_lastrequest;
-        auto dt = std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(dt_raw);
-        double elapsed_ms = dt.count();
+        // 2. infer which set are we at. we will only reach here if there is sth in buffer
+        dt_ms = chrono_to_double(std::chrono::high_resolution_clock::now() - ctrl_lastrequest);
+    
 
-        if (elapsed_ms > 20 && system_start)
+        if (!system_on && fsm == 1)
+        {
+            std::cout<<"FREDO1 STARTED...\n\n";
+            system_on = true;
+        }
+        
+        switch (fsm)
+        {
+        case 0:
+            // IDLE
+            input = "7777\n";
+            write(fd, input.c_str(), input.size());
+            break;
+
+        case 1:
+            // CTRL
+            input = "600-700-800\n";
+            // do ctrl shit
+            write(fd, input.c_str(), input.size());
+            break;
+
+        case 2:
+            // calibrate
+            input = "\n";
+            // do ctrl shit
+            // write(fd, input.c_str(), input.size());
+            break;
+        
+        default:
+            break;
+        }
+
+        if (dt_ms > 20 && system_on)
         {
             input = "1006\n";
             write(fd, input.c_str(), input.size());
 
             ctrl_lastrequest = std::chrono::high_resolution_clock::now();
         }             
-        
-        // std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 
-    std::cout<<"END!"<<std::endl;
+    int lala = 0;
+    while (system_on)
+    {
+        lala ++;
+        
+        input = "8888\n";
+        write(fd, input.c_str(), input.size());
+
+        int n = read(fd, buf, sizeof(buf) - 1);
+        if (n > 0) 
+        {
+            buf[n] = 0;
+            joints_callback(buf);
+        } 
+        else if (n == 0 || (errno == EAGAIN || errno == EWOULDBLOCK))         
+            continue;
+        else 
+        {
+            perror("read error");
+            break;
+        }
+
+        if (fsm == 0)
+            break;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));        
+    }
+    
+    std::cout<<"FREDO1 SHUTDOWN...\n\n"<<std::endl;
     close(fd);
 
     
@@ -106,18 +176,61 @@ void handle_sigint(int signum)
     stop_flag = 1;
 }
 
-void joint_callback()
-{
-
-}
-
 void joint_spinner()
 {
 
 }
 
-void process_pot(const char buf[])
+void joints_callback(const char buf[])
 {
-    std::cout << "Received here in small function: " << buf << std::endl;
+    message_from_down.clear();
+    std::string str(buf);         
+    std::stringstream ss(str);           
+    std::string token;
+
+    while (std::getline(ss, token, '-')) 
+    {
+        token.erase(std::remove_if(token.begin(), token.end(),
+                           [](unsigned char c) { return c == '\n' || c == '\r'; }),
+            token.end());
+
+        if (
+            is_digits(token)
+            // true
+        )
+        {
+            std::cout<<token<< " ";
+            message_from_down.emplace_back(std::stod(token));
+        }
+    }
+        
+            
+    std::cout <<std::endl << message_from_down.size()<<std::endl;
+
+    t_ard_ms = message_from_down[0];
+    fsm = message_from_down[1];
+    pots_raw[0] = message_from_down[2];
+    pots_raw[1] = message_from_down[3];
+    pots_raw[2] = message_from_down[4];
+
+    return;
 }
+
+bool is_digits(const std::string& s) {
+    try {
+        size_t idx;
+        std::stod(s, &idx);  // try parsing to double
+        return idx == s.size(); // ensure full string was parsed
+    } catch (...) {
+        return false;
+    }
+}
+
+
+double chrono_to_double(const std::chrono::nanoseconds time_chrono)
+{
+    // here we return milli sec in double
+    return std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(time_chrono).count();
+}
+
 
